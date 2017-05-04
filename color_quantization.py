48c0edaed1ad10b8a523a5fbb4d248f3f83115b5
Lego1st <nguyentrungnghia1812@gmail.com>
2017-05-04 09:58:07 +0700

    nghia

--- color_quantization.py
+++ color_quantization.py
@@ -2,9 +2,6 @@ from sklearn.cluster import MiniBatchKMeans
 import cv2
 import numpy as np 
 
-def showImg(quant):
-	cv2.imshow("xxx", quant)
-	cv2.waitKey()
 	
 def quantize(n_clusters, image):
 	"""
@@ -29,41 +26,3 @@ def quantize(n_clusters, image):
 
 
 	return quant
-
-def textLayers(quant):
-	clrs = []
-	for i in range(quant.shape[0]):
-		for j in range(quant.shape[1]):
-			if not np.any(clrs == quant[i][j]):
-				clrs.append(quant[i][j])
-	
-	idx = 0
-	new_img = quant.copy()
-	final_quant = quant.copy()
-	max_number_pix = 0
-	for clr in clrs:
-		for i in range(new_img.shape[0]):
-			for j in range(new_img.shape[1]):
-				if (quant[i][j] == clr).all():
-					new_img[i][j] = np.array([255, 255, 255])
-				else:
-					new_img[i][j] = np.array([0, 0, 0])
-		idx += 1
-		print(idx)
-		img = new_img[:,:,0]
-		kernel	=	np.ones((1,new_img.shape[0]),	np.uint8) * 255
-		img	=	cv2.dilate(img,	kernel,	iterations=1)
-		img	=	cv2.erode(img,	kernel,	iterations=1)
-		img	=	cv2.erode(img,	kernel,	iterations=1)
-
-		cnt = 0
-		for i in range(new_img.shape[0]):
-			for j in range(new_img.shape[1]):
-				if img[i][j] == 255:
-					cnt += 1
-		if cnt > max_number_pix:
-			max_number_pix = cnt
-			final_quant = new_img[:,:,0]
-		# cv2.imshow("xxx", np.hstack([new_img[:,:,0]	, img]))
-		# cv2.waitKey()
-	return final_quant
--- select_sample.py
+++ select_sample.py
@@ -1,7 +1,7 @@
 # import the necessary packages
 import argparse
 import cv2
- 
+import numpy as np
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
 refPt = []
@@ -27,7 +27,7 @@ def click_and_crop(event, x, y, flags, param):
  
 		# draw a rectangle around the region of interest
 		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-		cv2.imshow("image", image)
+		cv2.imshow(folder, image)
 
 def rot_img(img, deg):
 	rows, cols = img.shape[:2]
@@ -36,54 +36,66 @@ def rot_img(img, deg):
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
+# ap.add_argument("-i", "--image", required=True, help="Path to the image")
+# args = vars(ap.parse_args())
  
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread(args["image"])
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
- 
-rot = 0
-prev_rot = 0
-# keep looping until the 'q' key is pressed
+# # load the image, clone it, and setup the mouse callback function
+# image = cv2.imread(args["image"])
 
-while True:
-	# display the image and wait for a keypress
-	if rot != prev_rot:
-		print(rot)
-		prev_rot = rot
-		image = rot_img(clone.copy(), rot)
-	cv2.imshow("image", image)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the 'r' key is pressed, reset the cropping region
-	if key == ord("r"):
-		image = rot_img(clone.copy(), rot)
-		refPt.clear()
- 
-	# if the 'c' key is pressed, break from the loop
-	elif key == ord("c"):
-		if len(refPt) == 2:
+idx = 0
+image = np.array([])
+folder = "image"
+
+def select(img, fldr):
+	global image
+	global idx
+	global folder
+	folder = fldr
+	image = img.copy() 
+	clone = image.copy()
+	cv2.namedWindow(folder)
+	cv2.setMouseCallback(folder, click_and_crop)
+	rot = 0
+	prev_rot = 0
+	idx = 0
+	# keep looping until the 'q' key is pressed
+
+	while True:
+		# display the image and wait for a keypress
+		if rot != prev_rot:
+			print(rot)
+			prev_rot = rot
 			image = rot_img(clone.copy(), rot)
-			roi = image[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-			# cv2.imshow("ROI", roi)
-			# cv2.waitKey()
-			cv2.imwrite("images/6.png", roi)
+		cv2.imshow(folder, image)
+		key = cv2.waitKey(1) & 0xFF
+	 
+		# if the 'r' key is pressed, reset the cropping region
+		if key == ord("r"):
+			image = rot_img(clone.copy(), rot)
+			refPt.clear()
+	 
+		# if the 'c' key is pressed, break from the loop
+		elif key == ord("c"):
+			if len(refPt) == 2:
+				image = rot_img(clone.copy(), rot)
+				roi = image[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+				# cv2.imshow("ROI", roi)
+				# cv2.waitKey()
+				cv2.imwrite(folder + "/" + str(idx) + ".png", roi)
+				idx += 1
 
-	elif key == ord("a"):
-		rot += 1
+		elif key == ord("a"):
+			rot += 1
 
-	elif key == ord("d"):
-		rot -= 1
+		elif key == ord("d"):
+			rot -= 1
 
-	elif key == ord("q"):
-		break
- 
-# if there are two reference points, then crop the region of interest
-# from teh image and display it
+		elif key == ord("q"):
+			break
+	 
+	# if there are two reference points, then crop the region of interest
+	# from teh image and display it
 
- 
-# close all open windows
-cv2.destroyAllWindows()
+	 
+	# close all open windows
+	cv2.destroyAllWindows()
